3aaf22a63ade0c29f670b1a4983b0bece7eb710b
multiflexer <_>
2022-08-15 13:52:14 +0300

    Исправил Dockerfile для запуска с gunicorn

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9
 
-WORKDIR /app
+WORKDIR /app/src
 
 COPY ./requirements.txt /app/requirements.txt
 
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
 COPY ./src /app/src/
 
-CMD ["python3", "/app/src/main.py"]
+CMD gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 main:app
--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ from api.v1 import films, genres, persons
 from core.config import Config
 from db import elastic, redis
 
+config = Config()
 app = FastAPI(
-    title=Config.PROJECT_NAME,
+    title=config.PROJECT_NAME,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
@@ -18,8 +19,8 @@ app = FastAPI(
 
 @app.on_event('startup')
 async def startup():
-    redis.redis = await aioredis.create_redis_pool((Config.REDIS_HOST, Config.REDIS_PORT), minsize=10, maxsize=20)
-    elastic.es = AsyncElasticsearch(hosts=[f'{Config.ELASTIC_HOST}:{Config.ELASTIC_PORT}'])
+    redis.redis = await aioredis.create_redis_pool((config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
 
 
 @app.on_event('shutdown')
@@ -35,9 +36,9 @@ app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
 
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
-        port=8000,
-    )
+# if __name__ == '__main__':
+#     uvicorn.run(
+#         'main:app',
+#         host='0.0.0.0',
+#         port=8000,
+#     )
